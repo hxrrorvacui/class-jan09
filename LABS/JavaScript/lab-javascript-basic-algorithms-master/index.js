f93fac962ac5a90c387f2b9b40aabecafdf8afd6
“Laura <lauri@LAURIs-MacBook-Pro.local>
2023-01-15 20:15:10 +0100

    done

---
+++ LABS/JavaScript/lab-javascript-basic-algorithms-master/index.js
@@ -0,0 +1,16 @@
+// Iteration 1: Names and Input
+let hacker1 = "Darth Maul";
+console.log (`The driver's name is Darth Maul`);
+let hacker2 = "Poison Ivy"
+console.log(`The navigator's name is Poison Ivy`);
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The ${hacker1} has the longest name, it has ${hacker1.length} characters.`) 
+}
+else if (hacker2.length > hacker1.length) { 
+    console.log(`It seems that the navigatorhas the longest namem it has ${hacker2} characters.`)
+}
+else {
+    console.log(`Wow, you have both equally long names, ${hacker1.length+hacker2.length} characters!`)
+}
+// Iteration 3: Loops
