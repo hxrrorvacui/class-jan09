64e48019cd4aac79cd0c70c7b358a5fe9a785a25
“Laura <lauri@LAURIs-MacBook-Pro.local>
2023-01-16 11:09:02 +0100

    done

---
+++ CLASS PRACTICE/operators.js
@@ -0,0 +1,17 @@
+// Exercise 1
+let num;
+num = 1;
+console.log(num+1);
+console.log(valor += 1);
+console.log(num++);
+
+
+// Exercise 2
+const num = 5;
+const string = 5;
+console.log(num==string);
+console.log(num === string);
+console.log(num != string);
+console.log(num >=9);
+console.log(num === string || num<=5);
+
