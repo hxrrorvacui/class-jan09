6e38082fe04f71a5fcfb16315d65d9df540e46b3
“Laura <lauri@LAURIs-MacBook-Pro.local>
2023-01-17 14:33:14 +0100

    done

--- LABS/JavaScript/JS Basic Algorithms/index.js
+++ LABS/JavaScript/JS Basic Algorithms/index.js
@@ -14,3 +14,35 @@ else {
     console.log(`Wow, you have both equally long names, ${hacker1.length+hacker2.length} characters!`)
 }
 // Iteration 3: Loops
+let hacker1= "Darth Maul";
+let reservedName: 
+for (let i = 0; i <= hacker1.length; i++) {
+    reservedName = hacker1[i].toUpperCase() + "";
+}
+console.log(reservedName);
+
+
+// Diego version: 
+let hacker1 = "Darth Maul";
+console.log (hacker1.length);
+let hacker2 = "Poison Ivy"
+console.log(hacker2.length);
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The ${hacker1} has the longest name, it has ${hacker1.length} characters.`) 
+}
+    else if (hacker1.length < hacker2.length) {
+        console.log(`Wow, you have both equally long names, ${hacker1.length+hacker2.length} characters!`);
+    }
+console.log("Iteration 002 - Variation 001");
+
+//operador ternario // 
+hacker1.length > hacker2.length 
+? console.log(`The ${hacker1} has the longest name, it has ${hacker1.length} characters.`)
+: console.log(`Wow, you have both equally long names, ${hacker1.length+hacker2.length} characters!`); 
+
+// Loop
+let dirverName = "";
+for (const hackerName of newName) {
+    driverName += hackerName
+}
