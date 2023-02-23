f6ae02777cbc1b84843fe6aa80cded5874935b6a
Laura Anguita <anguita.lauraisa@gmail.com>
2023-02-23 15:33:11 +0100

    lab movies upload

---
+++ LABS/lab-javascript-greatest-movies-master/src/movies.js
@@ -0,0 +1,77 @@
+// Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(moviesArray) {
+    let allDirectors = []
+    for (let i = 0; i < moviesArray.length; i++){
+     allDirectors.push(moviesArray[i].director);
+    }
+   return [...new Set(allDirectors)];
+  };
+  console.log(getAllDirectors(movies));
+
+
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+function howManyMovies(moviesArray) {
+    
+    let filteredMovies = moviesArray.filter( e => {
+        return e.director === "Steven Spielberg" && e.genre.includes("Drama")});
+    return filteredMovies.length;
+}
+
+// Iteration 3: All scores average - Get the average of all scores with 2 decimals
+function scoresAverage(moviesArray) {
+    if (!moviesArray.length){
+        return 0;
+    }
+        let scores = [];
+    for (let i = 0; i < moviesArray.length; i++) {
+        if (!moviesArray[i].score){
+            scores.push(0);
+        } else {
+        scores.push(moviesArray[i].score);  
+    }
+}
+    let scoresAverage = scores.reduce((a, b) =>  a + b)/scores.length;
+    // console.log(scores);
+    return Number(scoresAverage.toFixed(2));
+}
+
+//this is a shorter version:
+
+    const scoresAverage2 = (moviesArray) => {
+        if (!movies.length) return 0;
+        const totalScore = moviesArray.reduce((accumulator, currentValue) => {
+            if (currentValue.score) {
+                return accumulator + currentValue.score;
+            } else { 
+                return accumulator
+            }
+        }, 0);
+        return Number((totalScore/moviesArray.length)).toFixed(2);
+    }
+
+// Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesScore(moviesArray) {
+    let dramaMovies = moviesArray.filter( e => {
+        return e.genre.includes("Drama")});
+    return scoresAverage(dramaMovies);
+}
+console.log(dramaMoviesScore(movies))
+
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(moviesArray) {
+let sortedByYear = moviesArray.slice(0).sort((one, two) => {
+    if (one.year > two.year) return one.year - two.year
+    if (one.year < two.year) return one.year - two.year
+    else return one.title.localeCompare(two.title);
+  });
+  return sortedByYear;
+};
+console.table(orderByYear(movies));
+
+// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(moviesArray) {
+    let movieTitles = moviesArray.map((movie) => movie.title);
+    let sortByTitle = movieTitles.sort((a, b) => a > b ? 1 : -1);
+    return sortByTitle.slice(0,20);
+}
+console.log(orderAlphabetically(movies))
