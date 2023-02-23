4930a210d408bdfcc1c6f662a4037b8e589d999d
Laura Anguita <anguita.lauraisa@gmail.com>
2023-02-23 14:51:55 +0100

    final version

---
+++ LABS/lab-vue-c-wiki-countries/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import NavigationHeader from "./components/NavigationHeader.vue";
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <NavigationHeader />
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
